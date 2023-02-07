c7440ab6fbd875f21c28a6b53230aae148992f94
pjoon357 <pjoon357@naver.com>
2023-02-07 17:55:39 +0900

    init

---
+++ src/main/java/com/example/foodroads/FoodRoadsApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodroads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodRoadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodRoadsApplication.class, args);
+	}
+
+}
