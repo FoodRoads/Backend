a806eacdc62fff148ec82b12e90695ae89acabbf
pjoon357 <pjoon357@naver.com>
2023-02-14 15:43:21 +0900

    feat: findAll

--- src/main/java/com/example/foodroads/domain/auth/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/foodroads/domain/auth/repository/RefreshTokenRepository.java
@@ -3,8 +3,10 @@ package com.example.foodroads.domain.auth.repository;
 import com.example.foodroads.domain.auth.entity.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
+   List<RefreshToken> findAll();
    Optional<RefreshToken> findByRefreshToken(String refreshToken);
 }
