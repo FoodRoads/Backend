a03b23eb3c366a067c28b5df1b88e4a55d4f0522
pjoon357 <pjoon357@naver.com>
2023-03-01 22:43:48 +0900

    feat: CI/CD 분리

--- src/test/java/com/example/foodroads/integrationTest/IntegrationTest.java
+++ src/test/java/com/example/foodroads/integrationTest/IntegrationTest.java
@@ -2,11 +2,11 @@ package com.example.foodroads.integrationTest;
 
 import com.example.foodroads.FoodRoadsApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = {FoodRoadsApplication.class})
-@ActiveProfiles("dev")
+@ActiveProfiles("test")
+@Import({TestRedisConfig.class})
 public abstract class IntegrationTest {
-
-
 }
